aedd8d34e10635b22988fdf81492c1f2ebd0effd
Neaz-mq <neazmorshed666@gmail.com>
2024-04-28 11:07:47 +0600

    update index

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+
 // middlewares
 app.use(cors());
 app.use(express.json());
 
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zyo6s3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,33 +23,37 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
-    
+
     const templateCollection = client.db("templateDb").collection("template");
     const freeCollection = client.db("templateDb").collection("free");
     const testimonialsCollection = client.db("templateDb").collection("testimonials");
 
+
     // template related apis
 
     app.get('/template', async (req, res) => {
-        const result = await templateCollection.find().toArray();
-        res.send(result);
-      });
+      const result = await templateCollection.find().toArray();
+      res.send(result);
+    });
 
-      app.get('/template/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
+    app.get('/template/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
 
-        const options = {
-          // Include only the `title` and `imdb` fields in the returned document
-          projection: { name: 1, price: 1,  image: 1 },
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { name: 1, price: 1, image: 1 },
       };
 
-        const result = await templateCollection.findOne(query, options);
-        res.send(result);
+      const result = await templateCollection.findOne(query, options);
+      res.send(result);
     });
+
 
     // free template related apis
 
@@ -63,28 +68,24 @@ async function run() {
 
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { name: 1, price: 1,  image: 1 },
-    };
+        projection: { name: 1, price: 1, image: 1 },
+      };
 
       const result = await freeCollection.findOne(query, options);
       res.send(result);
-  });
+    });
 
 
-
-
-   
     // testimonials related apis
+    
     app.get('/testimonials', async (req, res) => {
       const result = await testimonialsCollection.find().toArray();
       res.send(result);
     });
 
-   
-
 
     // Send a ping to confirm a successful connection
-   
+
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -96,9 +97,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Templates are here')
-  })
-   
-  app.listen(port, () => {
-    console.log(`Template store is live on port ${port}`);
-  })
+  res.send('Templates are here')
+})
+
+app.listen(port, () => {
+  console.log(`Template store is live on port ${port}`);
+})
