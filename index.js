c5e26bb726092f1dca12c61fee13d8c7659cf16c
Neaz-mq <neazmorshed666@gmail.com>
2024-05-04 11:15:51 +0600

    cart collection work added nav

--- index.js
+++ index.js
@@ -85,6 +85,12 @@ async function run() {
 
      // cart collection apis
 
+
+     app.get('/carts', async (req, res) => {
+       const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
      app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem);
