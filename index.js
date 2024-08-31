e9ead3e8cf7ff12cad39b550f23caec53b6318e2
Neaz-mq <neazmorshed666@gmail.com>
2024-08-31 18:10:01 +0600

    Template update

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const options = {
 
-        projection: { type: 1, category: 1, price: 1, image: 1,  description: 1, specifications: 1, product: 1, files: 1, picture: 1 },
+        projection: { type: 1, category: 1, price: 1, image: 1,  description: 1, specifications: 1, product: 1, documents: 1, picture: 1, revisions: 1, files: 1 },
       };
 
       const result = await templateCollection.findOne(query, options);
@@ -266,8 +266,10 @@ async function run() {
           description: temp.description,
           specifications: temp.specifications,
           product: temp.product,
+          revisions: temp.revisions,
+          documents: temp.documents,
+          picture: temp.picture,
           files: temp.files,
-          picture: temp.picture
         }
       }
 
