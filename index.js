350073047eec41e5ca7a3d1c730496545d75ce95
Neaz-mq <neazmorshed666@gmail.com>
2024-09-04 13:16:02 +0600

    addfreetemplate  update

--- index.js
+++ index.js
@@ -297,7 +297,7 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const options = {
         // Include only the `title` and `imdb` fields in the returned document
-        projection: { type: 1, category: 1, price: 1, image: 1,  description: 1, specifications: 1, product: 1, files: 1, picture: 1 },
+        projection: { type: 1, category: 1, price: 1, image: 1,  description: 1, specifications: 1, product: 1, documents: 1, picture: 1, revisions: 1, files: 1 },
       };
       const result = await freeCollection.findOne(query, options);
       res.send(result);
@@ -325,8 +325,10 @@ async function run() {
           description: temp.description,
           specifications: temp.specifications,
           product: temp.product,
+          revisions: temp.revisions,
+          documents: temp.documents,
+          picture: temp.picture,
           files: temp.files,
-          picture: temp.picture
         }
       }
 
