7368eded96858542d6304df6d3fe31055b06dbd0
neazmorshed666@gmail.com <neazmorshed666@gmail.com>
2024-11-09 11:38:46 +0600

    download the template file

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const port = process.env.PORT || 5000;
 const SSLCommerzPayment = require('sslcommerz-lts');
 
 
+// Increase payload size limit (example: 50MB)
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // middlewares
 
